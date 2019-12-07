79ce1736cad504157b13b519f1cbc1a941b60567
jiayi <131472776@163.com>
2019-12-07 11:06:49 +0800

    塞选和分页

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -28,10 +28,37 @@ function changePage(page){
     success:function(response){
         console.log(response);
       var html = template('postsTpl', response);
-    //   console.log(html);
       $('#postsBox').html(html)
       var page = template('pageTpl',response)
       $('#page').html(page)
     }
 })
 }
+//向服务器端发送请求 索要分类数据
+$.ajax({
+  type:'get',
+  url:'/categories',
+  success:function(response){
+      console.log(response);
+      var html = template('categoryTpl',{data:response});
+      $('#categoryBox').html(html)   
+  }
+})
+//当点击筛选的时候
+$('#filterForm').on('submit',function(){
+  //获取管理选择的过滤条件
+  var formData = $(this).serialize();
+  //向服务器端发送请求 根据条件索要文件列表数据
+  $.ajax({
+    type:'get',
+    url:'/posts',
+    data:formData,
+    success:function(response){
+      var html = template('postsTpl',response);
+      $('#postsBox').html(html);
+      var page = template('pageTpl',response);
+      $('#page').html(page)
+    }
+  })
+  return false
+})
