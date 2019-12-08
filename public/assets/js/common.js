af30cb6c17d6a5d1f27fc6741b13bbc725afa0ed
jiayi <131472776@163.com>
2019-12-08 16:47:54 +0800

    用户信息显示功能

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -29,5 +29,14 @@ function formateDate(date){
   //将日期时间字符串转换成日期对象
   date = new Date(date);
   return   date.getFullYear() + '-'+(date.getMonth()+1) +'-'+ date.getDate();
-  
 };
+// 向服务器发送请求 索要登录用户信息
+$.ajax({
+  type: 'get',
+  url: '/users/' + userId,
+  success: function (response) {
+    console.log(response);
+    $('.avatar').attr('src',response.avatar)
+    $('.profile .name').html(response.nickName)
+  } 
+})
